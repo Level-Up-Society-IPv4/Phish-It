0ba4ed63e75a5d265bac275b48f6ec55441728c2
Tim McCabe <me@timmcca.be>
2021-07-16 22:44:11 -0400

    Add quiz intro page

--- src/quiz/Quiz.js
+++ src/quiz/Quiz.js
@@ -1,68 +1,13 @@
-import styles from './Quiz.module.css';
-import { useState } from 'react';
-import man from './assets/man.png';
-import { Select } from './components/Select';
-import { FROM_OPTIONS, getSubjectOptions } from '../options';
+import { useState } from "react"
+import { EmailBuilder } from "./EmailBuilder";
+import { QuizIntro } from "./QuizIntro";
 
 export const Quiz = () => {
-  const [from, setFrom] = useState(null);
-  const [subject, setSubject] = useState(null);
-  const points = subject?.points + from?.points;
+  const [startedQuiz, setStartedQuiz] = useState(false);
 
-  const showSubject = from !== null;
-
-  return (
-    <>
-      <div className={styles.App}>
-        <div className={styles.labelGroup}>
-          <div className={styles.label}>
-            <p className={styles.labelText}>To:</p>
-          </div>
-          <div className={styles.label}>
-            <img src={man} className={styles.man} alt="John" />
-            <p className={styles.labelText}>John@SeventhNinthBank.com</p>
-          </div>
-        </div>
-        <div className={styles.labelGroup}>
-          <div className={styles.label}>
-            <p className={styles.labelText}>From:</p>
-          </div>
-          {
-            from ? (
-              <div className={styles.label}>
-                <p className={styles.labelText}>{from.text}</p>
-              </div>
-            ) : (
-              <Select
-                options={FROM_OPTIONS}
-                nextCallback={setFrom}
-              />
-            )
-          }
-        </div>
-        <div className={styles.labelGroup}>
-          <div className={styles.label}>
-            <p className={styles.labelText}>Subject:</p>
-          </div>
-          {
-            showSubject && (
-              subject ? (
-                <div className={styles.label}>
-                  <p className={styles.labelText}>{subject.text}</p>
-                </div>
-              ) : (
-                <Select
-                  options={getSubjectOptions(from.id)}
-                  nextCallback={setSubject}
-                />
-              )
-            )
-          }
-        </div>
-      </div>
-      {
-        subject && <h1>Score: {points}</h1>
-      }
-    </>
+  return startedQuiz ? (
+    <EmailBuilder />
+  ) : (
+    <QuizIntro startQuiz={() => setStartedQuiz(true)} />
   );
 }
---
+++ src/quiz/QuizIntro.js
@@ -0,0 +1,15 @@
+import styles from './QuizIntro.module.css';
+import man from './assets/man.png';
+
+export const QuizIntro = ({ startQuiz }) => (
+  <div className={styles.container}>
+    <h1>Phishing Challenge 1</h1>
+    <img className={styles.man} src={man} alt="Man" width={300} height={300} />
+    <p className={styles.text}>
+      John is a middle manager working at Seventh Ninth Bank.
+      His Gmail has lots of sensitive financial documents that you want to swipe.
+      Cast your line and reel in his password!
+    </p>
+    <button className={styles.start} onClick={startQuiz}>Start</button>
+  </div>
+);
