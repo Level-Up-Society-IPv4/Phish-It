5298c3b30eeeb6e51e535ebf658cccff45a4f16d
Tim McCabe <me@timmcca.be>
2021-07-16 22:13:23 -0400

    Set up React Router

--- src/App.js
+++ src/App.js
@@ -1,68 +1,22 @@
-import styles from './App.module.css';
-import { useState } from 'react';
-import man from './assets/man.png';
-import { Select } from './components/Select';
-import { FROM_OPTIONS, getSubjectOptions } from './options';
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import { Home } from "./home/Home";
+import { Quiz } from "./quiz/Quiz";
 
-export const App = () => {
-  const [from, setFrom] = useState(null);
-  const [subject, setSubject] = useState(null);
-  const points = subject?.points + from?.points;
-
-  const showSubject = from !== null;
-
-  return (
-    <>
-      <div className={styles.App}>
-        <div className={styles.labelGroup}>
-          <div className={styles.label}>
-            <p className={styles.labelText}>To:</p>
-          </div>
-          <div className={styles.label}>
-            <img src={man} className={styles.man} alt="John" />
-            <p className={styles.labelText}>John@SeventhNinthBank.com</p>
-          </div>
-        </div>
-        <div className={styles.labelGroup}>
-          <div className={styles.label}>
-            <p className={styles.labelText}>From:</p>
-          </div>
-          {
-            from ? (
-              <div className={styles.label}>
-                <p className={styles.labelText}>{from.text}</p>
-              </div>
-            ) : (
-              <Select
-                options={FROM_OPTIONS}
-                nextCallback={setFrom}
-              />
-            )
-          }
-        </div>
-        <div className={styles.labelGroup}>
-          <div className={styles.label}>
-            <p className={styles.labelText}>Subject:</p>
-          </div>
-          {
-            showSubject && (
-              subject ? (
-                <div className={styles.label}>
-                  <p className={styles.labelText}>{subject.text}</p>
-                </div>
-              ) : (
-                <Select
-                  options={getSubjectOptions(from.id)}
-                  nextCallback={setSubject}
-                />
-              )
-            )
-          }
-        </div>
-      </div>
-      {
-        subject && <h1>Score: {points}</h1>
-      }
-    </>
-  );
-}
+export const App = () => (
+  <BrowserRouter>
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/quiz">Quiz</Link>
+      </nav>
+      <Switch>
+        <Route path="/quiz">
+          <Quiz />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
