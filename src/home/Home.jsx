fe749ed716c85149702a43771cdd0f813cc30022
benyoss <benyoss4@gmail.com>
2021-07-17 09:05:23 -0500

    (finish) Layout of homepage component

---
+++ src/home/Home.jsx
@@ -0,0 +1,16 @@
+import logo from './assets/Phisher-logo.png';
+import styles from './Home.module.css';
+import { Link } from 'react-router-dom';
+export const Home = () => (
+  <div className={styles.container}>
+    <div className={styles.iconBody}>
+    <Link to="/login">
+      <div className={styles.icon}>
+      </div>
+    </Link>
+    </div>
+    <div className={styles.homepageBody}>
+      <h1 className={styles.homepageTitle}>PhishMe!</h1>
+    </div>
+  </div>
+);
