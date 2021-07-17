ad70aaf8cd33ed2cc564f78173577333d0e96820
Mansi <mansijpatel321@gmail.com>
2021-07-17 18:10:35 -0400

    Add login page design

---
+++ src/login/Login.jsx
@@ -0,0 +1,11 @@
+import styles from './login.module.css';
+import { Link } from 'react-router-dom';
+
+export const Login = () => {
+    <div id="homepage">
+        <div id="skybluebackground">
+            <h3>testing</h3>
+        </div>
+    </div>
+    return <h1>Login</h1>;
+};
