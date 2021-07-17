34765f875fd15802ff79d50668b224809c5bc8b3
Mansi <mansijpatel321@gmail.com>
2021-07-17 18:20:34 -0400

    update login page design

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,11 +1,23 @@
-import styles from './login.module.css';
+import styles from './Login.module.css';
 import { Link } from 'react-router-dom';
 
-export const Login = () => {
+export const Login = () => (
     <div id="homepage">
         <div id="skybluebackground">
+            <div id="logintitle">Login</div>
+            <div id="email">Email</div>
+            <div id="textbox1"></div>
+            <div id="password">Password</div>
+            <div id="textbox2"></div>
+            <div id="yellowrect2">
+                <div id="login">Login</div>
+            </div>
+            <div id="noaccount">No Account?</div>
+            <div id="yellowrect1">
+                <div id="companyaccount">Create Company Account</div>
+            </div>
+            
             <h3>testing</h3>
         </div>
     </div>
-    return <h1>Login</h1>;
-};
+);
