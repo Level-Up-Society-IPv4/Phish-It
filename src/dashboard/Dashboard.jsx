6ae8c5faec58c9c3c99179f2c9d952059840682a
Mansi <mansijpatel321@gmail.com>
2021-07-18 01:41:40 -0400

    Adding purchase coins option

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -25,6 +25,11 @@ export const Dashboard = () => {
                             <th>
                                 <h3 className={styles.player}>Group: Phishermen</h3>
                             </th>
+                            <th>
+                                <button className={styles.player}>
+                                    <img src="..assets/coin.png" className={styles.coin}></img> Get more coins!
+                                </button>
+                            </th>
                             <table className={styles.progressBar}>
                                 <EXPContext.Consumer>
                                     {
