dc03b1d1b8dbfd55ab36c1e13e9f0fc7ef65ec10
Mansi <mansijpatel321@gmail.com>
2021-07-17 20:15:32 -0400

    Updating login page design

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -2,21 +2,19 @@ import styles from './Login.module.css';
 
 export const Login = () => (
     <div id={styles.homepage}>
-        <div id="skybluebackground">
-            <div id="logintitle">Login</div>
-            <div id="email">Email</div>
-            <div id="textbox1"></div>
-            <div id="password">Password</div>
-            <div id="textbox2"></div>
-            <div id="yellowrect2">
-                <div id="login">Login</div>
+        <div id={styles.skybluebackground}>
+            <div id={styles.logintitle}>Login</div>
+            <div id={styles.email}>Email</div>
+            <div id={styles.textbox1}></div>
+            <div id={styles.password}>Password</div>
+            <div id={styles.textbox2}></div>
+            <div id={styles.yellowrect2}>
+                <div id={styles.login}>Login</div>
             </div>
-            <div id="noaccount">No Account?</div>
-            <div id="yellowrect1">
-                <div id="companyaccount">Create Company Account</div>
+            <div id={styles.noaccount}>No Account?</div>
+            <div id={styles.yellowrect1}>
+                <div id={styles.companyaccount}>Create Company Account</div>
             </div>
-            
-            <h3>testing</h3>
-        </div>
+        </div>   
     </div>
 );
