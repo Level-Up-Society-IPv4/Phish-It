49f577825c52af440b336560cc12652d173b5e37
Mansi <mansijpatel321@gmail.com>
2021-07-17 20:28:22 -0400

    update login page design

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -2,19 +2,16 @@ import styles from './Login.module.css';
 
 export const Login = () => (
     <div id={styles.homepage}>
-        <div id={styles.skybluebackground}>
-            <div id={styles.logintitle}>Login</div>
-            <div id={styles.email}>Email</div>
-            <div id={styles.textbox1}></div>
-            <div id={styles.password}>Password</div>
-            <div id={styles.textbox2}></div>
-            <div id={styles.yellowrect2}>
-                <div id={styles.login}>Login</div>
-            </div>
-            <div id={styles.noaccount}>No Account?</div>
-            <div id={styles.yellowrect1}>
-                <div id={styles.companyaccount}>Create Company Account</div>
-            </div>
-        </div>   
+        <div id={styles.skybluebackground}></div>
+        <div id={styles.logintitle}>Login</div>
+        <div id={styles.email}>Email</div>
+        <div id={styles.textbox1} type="textbox"></div>
+        <div id={styles.password}>Password</div>
+        <div id={styles.textbox2} type="textbox"></div>
+        <div id={styles.yellowrect2}></div>
+        <div id={styles.login} type="button">Login</div>
+        <div id={styles.noaccount}>No Account?</div>
+        <div id={styles.yellowrect1}></div>           
+        <div id={styles.companyaccount}>Create Company Account</div>      
     </div>
 );
