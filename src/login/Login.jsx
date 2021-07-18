9019ac4f46f74c2f670e4239614a735c956cfa23
Mansi <mansijpatel321@gmail.com>
2021-07-17 20:43:11 -0400

    Update login page design

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,4 +1,5 @@
 import styles from './Login.module.css';
+import {Link} from 'react-router-dom';
 
 export const Login = () => (
     <div id={styles.homepage}>
@@ -9,9 +10,12 @@ export const Login = () => (
         <div id={styles.password}>Password</div>
         <div id={styles.textbox2} type="textbox"></div>
         <div id={styles.yellowrect2}></div>
-        <div id={styles.login} type="button">Login</div>
+        <Link to="/quizintro">
+            <div className={styles.login}>Login</div>
+        </Link>
         <div id={styles.noaccount}>No Account?</div>
         <div id={styles.yellowrect1}></div>           
         <div id={styles.companyaccount}>Create Company Account</div>      
     </div>
+
 );
